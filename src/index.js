c274df36944346c140ca7ffdb7ef4539f4f70c63
Gregori Poloni <gregori.p.m@hotmail.com>
2023-05-01 17:01:12 -0300

    Add Shop Route and Products Cards

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import reportWebVitals from './reportWebVitals';
+import { ProductsProvider } from './contexts/products.context';
 
 import './index.scss';
 
@@ -13,7 +14,9 @@ root.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<UserProvider>
-				<App />
+				<ProductsProvider>
+					<App />
+				</ProductsProvider>
 			</UserProvider>
 		</BrowserRouter>
 	</React.StrictMode>
