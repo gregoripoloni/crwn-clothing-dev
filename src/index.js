5fc659206f31a9ee64278c77ecd672b4bf62e7d0
Gregori Poloni <gregori.p.m@hotmail.com>
2024-02-06 22:16:14 -0300

    Add User store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './store/store'
 
 import App from './App'
 import { CategoriesProvider } from './contexts/categories.context'
 import { CartProvider } from './contexts/cart.context'
+import { store } from './store/store'
 
 import './index.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
 
 root.render(
 	<React.StrictMode>
