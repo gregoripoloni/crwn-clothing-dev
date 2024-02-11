8a0c2ad263564a1290b6ba8be3b239793dfe1a01
Gregori Poloni <gregori.p.m@hotmail.com>
2024-02-11 20:55:58 -0300

    Add ReduxThunk

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -1,17 +1,23 @@
 import { Fragment } from 'react'
 import { useSelector } from 'react-redux'
 
-import { selectCategoriesMap } from '../../store/categories/category.selector'
+import { selectCategoriesMap, selectIsCategoriesIsLoading } from '../../store/categories/category.selector'
 
 import CategoryPreview from '../../components/category-preview/category-preview.component'
+import Spinner from '../../components/spinner/spinner.component'
 
 const CategoriesPreview = () => {
 	const categoriesMap = useSelector(selectCategoriesMap)
+	const isLoading = useSelector(selectIsCategoriesIsLoading)
 
 	return (
 		<Fragment>
-			{Object.keys(categoriesMap).map(title =>
-				<CategoryPreview key={title} title={title} products={categoriesMap[title]} />
+			{ isLoading ? (
+				<Spinner />
+			) : (
+				Object.keys(categoriesMap).map(title =>
+					<CategoryPreview key={title} title={title} products={categoriesMap[title]} />
+				)
 			)}
 		</Fragment>
 	)
